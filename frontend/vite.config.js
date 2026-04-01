8faf9f7e3ffcc39beef6ea38d391c413fb78698b
Sherry-maxxigx <594380521@qq.com>
2026-04-02 01:10:25 +0800

    deploy frontend to github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ai-screenplay-system/',
   plugins: [vue()],
   server: {
     port: 3000,
